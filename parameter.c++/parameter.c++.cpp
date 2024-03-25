2491f423221e74ba54d14edfed2b27f696b2549e
fisabil23 <161275234+fisabil23@users.noreply.github.com>
2024-03-25 10:55:52 +0700

    Add project files.

---
+++ parameter.c++/parameter.c++.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using  namespace std;
+
+int a, b;
+
+
+void input()
+
+{
+    cout << " bilangan pertama = ";
+    cin >> a;
+
+    cout << " bilangan kedua = ";
+    cin >> b;
+
+}
+
+int penjumlahan(int bil_1, int bil_2) {
+    return bil_1 + bil_2;
+}
+
+int pengurangan(int bil_1, int bil_2) {
+    return bil_1 - bil_2;
+}
+
+int perkalian(int bil_1, int bil_2) {
+    return bil_1 * bil_2;
+}
+
+int pembagian(int bil_1, int bil_2) {
+    return bil_1 / bil_2;
+}
+
+int main()
+{
+    int pilihan;
+
+    do {
+        system("cls");
+        cout << "=========menu======" << endl;
+        cout << "1. penjumlahan" << endl;
+        cout << "2. pengurangan" << endl;
+        cout << "3. perkalian" << endl;
+        cout << "4. pembagian" << endl;
+        cout << "5.keluar" << endl;
+        cout << "masukan menu yang ingin di pilih (1/2/3/4/5):";
+        cin >> pilihan;
+
+        switch (pilihan)
+        {
+        case 1:
+            input();
+            cout << " hasil penjumlahan = " << penjumlahan(a, b) << endl;
+            system("pause");
+            break;
+        case 2:
+            input();
+            cout << " hasil pengurangan = " << pengurangan(a, b) << endl;
+            system("pause");
+            break;
+        case 3:
+            input();
+            cout << " hasil perkalian = " << perkalian(a, b) << endl;
+            system("pause");
+            break;
+        case 4:
+            input();
+            cout << " hasil pembagian = " << pembagian(a, b) << endl;
+            system("pause");
+            break;
+
+
+        }
+    } while (pilihan != 5);
+} 
